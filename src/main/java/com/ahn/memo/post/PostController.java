2870e24cbc2f862245405da1fb32f2dfa016b2ea
AHN <dsfs3975@naver.com>
2023-02-09 19:35:28 +0900

    [안재원]  - 로그아웃 기능 추가
    nakk3975/Memo#2

---
+++ src/main/java/com/ahn/memo/post/PostController.java
@@ -0,0 +1,15 @@
+package com.ahn.memo.post;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/post")
+public class PostController {
+
+	@GetMapping("/list/view")
+	public String listView() {
+		return "post/list";
+	}
+}
